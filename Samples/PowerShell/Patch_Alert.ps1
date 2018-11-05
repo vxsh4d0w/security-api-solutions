ef1e7f9186cc1090d5fa84d936275adb64cea51a
Edward Koval <ekoval@uw.edu>
2018-11-05 11:13:11 -0800

    Edwardkoval patch 1 (#2)
    * Create ISSUE_TEMPLATE
    
    * added powershell sample
    
    * moved into Samples
    
    * edit
    
    * edit
    
    * edit
    
    * edit

---
+++ Samples/PowerShell/Patch_Alert.ps1
@@ -0,0 +1,280 @@
+﻿<#
+
+.COPYRIGHT
+Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+See LICENSE in the project root for license information.
+
+#>
+
+####################################################
+
+function Get-AuthToken {
+
+<#
+.SYNOPSIS
+This function is used to authenticate with the Graph API REST interface
+.DESCRIPTION
+The function authenticate with the Graph API Interface with the tenant name
+.EXAMPLE
+Get-AuthToken
+Authenticates you with the Graph API interface
+.NOTES
+NAME: Get-AuthToken
+#>
+
+[cmdletbinding()]
+
+param
+(
+    [Parameter(Mandatory=$true)]
+    $User
+)
+
+    $userUpn = New-Object "System.Net.Mail.MailAddress" -ArgumentList $User
+
+    $tenant = $userUpn.Host
+
+    Write-Host "Checking for AzureAD module..."
+
+    $AadModule = Get-Module -Name "AzureAD" -ListAvailable
+
+    if ($AadModule -eq $null) {
+        Write-Host "AzureAD PowerShell module not found, looking for AzureADPreview"
+        $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable
+    }
+    if ($AadModule -eq $null) {
+        write-host
+        write-host "AzureAD Powershell module not installed..." -f Red
+        write-host "Install by running 'Install-Module AzureAD' or 'Install-Module AzureADPreview' from an elevated PowerShell prompt" -f Yellow
+        write-host "Script can't continue..." -f Red
+        write-host
+        exit
+    }
+
+    # Getting path to ActiveDirectory Assemblies
+    # If the module count is greater than 1 find the latest version
+    if($AadModule.count -gt 1){
+        $Latest_Version = ($AadModule | select version | Sort-Object)[-1]
+        $aadModule = $AadModule | ? { $_.version -eq $Latest_Version.version }
+
+        # Checking if there are multiple versions of the same module found
+        if($AadModule.count -gt 1){
+            $aadModule = $AadModule | select -Unique
+        }
+        $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+        $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
+    }
+    else {
+        $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+        $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
+    }
+
+    [System.Reflection.Assembly]::LoadFrom($adal) | Out-Null
+
+    [System.Reflection.Assembly]::LoadFrom($adalforms) | Out-Null
+
+    $clientId = "ENTER_YOUR_APP_ID_HERE"
+
+    $redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+
+    $resourceAppIdURI = "https://graph.microsoft.com"
+
+    $authority = "https://login.microsoftonline.com/$Tenant"
+
+    try {
+        $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
+
+        # https://msdn.microsoft.com/en-us/library/azure/microsoft.identitymodel.clients.activedirectory.promptbehavior.aspx
+        # Change the prompt behaviour to force credentials each time: Auto, Always, Never, RefreshSession
+
+        $platformParameters = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters" -ArgumentList "Auto"
+
+        $userId = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserIdentifier" -ArgumentList ($User, "OptionalDisplayableId")
+
+        $authResult = $authContext.AcquireTokenAsync($resourceAppIdURI,$clientId,$redirectUri,$platformParameters,$userId).Result
+
+        # If the accesstoken is valid then create the authentication header
+        if($authResult.AccessToken){
+
+            # Creating header for Authorization token
+            $authHeader = @{
+                'Content-Type'='application/json'
+                'Authorization'="Bearer " + $authResult.AccessToken
+                'ExpiresOn'=$authResult.ExpiresOn
+                'Prefer'='return=representation' # Prefer Header returns the updated alert
+                }
+            return $authHeader
+        }
+        else {
+            Write-Host
+            Write-Host "Authorization Access Token is null, please re-run authentication..." -ForegroundColor Red
+            Write-Host
+            break
+        }
+    }
+    catch {
+        write-host $_.Exception.Message -f Red
+        write-host $_.Exception.ItemName -f Red
+        write-host
+        break
+    }
+}
+
+####################################################
+
+Function Test-JSON(){
+
+<#
+.SYNOPSIS
+This function is used to test if the JSON passed to a REST Post request is valid
+.DESCRIPTION
+The function tests if the JSON passed to the REST Post is valid
+.EXAMPLE
+Test-JSON -JSON $JSON
+Test if the JSON is valid before calling the Graph REST interface
+.NOTES
+NAME: Test-JSON
+#>
+
+param (
+$JSON
+)
+
+    try {
+        $UpdateJsonJSON = ConvertFrom-Json $JSON -ErrorAction Stop
+        $validJson = $true
+    } 
+    catch {
+        $validJson = $false
+        $_.Exception
+    }
+    if (!$validJson){
+        Write-Host "Provided JSON isn't in valid JSON format" -f Red
+        break
+    }
+}
+
+####################################################
+
+Function Patch-Alert(){
+
+<#
+.SYNOPSIS
+This function is used to Patch a Security Alert from the Graph API REST interface
+.DESCRIPTION
+The function connects to the Graph API Interface and Updates a Security API Alert object
+.EXAMPLE
+Patch-Alert -JSON $JSON -ID $alert_Id
+.NOTES
+NAME: Patch-Alert
+#>
+
+[cmdletbinding()]
+
+param
+(
+    [Parameter(Mandatory=$true)]
+    $ID,
+    $JSON
+)
+
+    $graphApiVersion = "v1.0"
+    $Resource = "security/alerts/$ID"
+    try {
+        if(!$JSON){
+            write-host "No JSON was passed to the function, provide a JSON variable" -f Red
+            break
+        }
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+        if($JSON -notlike "*vendorInformation*"){
+
+            #Make a call to get alert before Update.
+            $oldAlert = (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get)
+
+            # Convert to powershell object to modify.
+            $UpdateJson = $JSON | ConvertFrom-Json
+
+            # Add required vendorInformation to the JSON.
+            $UpdateJson | Add-Member -Name vendorInformation -value $oldAlert.vendorInformation -MemberType NoteProperty
+
+            # Convert back to JSON
+            $UpdateJson = $UpdateJson | ConvertTo-Json
+            write-host "Added vendorInformation to the request Body:`n" $UpdateJson
+
+            $JSON = $UpdateJson
+        }
+
+        #validate the JSON format
+        Test-JSON -JSON $JSON
+
+        # Send PATCH request to update the alert.
+        Invoke-RestMethod -Uri $uri -Headers $authToken -Method Patch -Body $Json -ContentType "application/json"  
+
+        write-host
+        write-host "Alert has been updated."
+        write-host
+    } 
+    catch {
+        $ex = $_.Exception
+        $errorResponse = $ex.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($errorResponse)
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd();
+        Write-Host "Response content:`n$responseBody" -f Red
+        Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+        write-host
+        break
+    }
+}
+
+####################################################
+
+#region Authentication
+
+write-host
+
+# Checking if authToken exists before running authentication
+if($global:authToken){
+
+    # Setting DateTime to Universal time to work in all timezones
+    $DateTime = (Get-Date).ToUniversalTime()
+
+    # If the authToken exists checking when it expires
+    $TokenExpires = ($authToken.ExpiresOn.datetime - $DateTime).Minutes
+    if($TokenExpires -le 0){
+        write-host "Authentication Token expired" $TokenExpires "minutes ago" -ForegroundColor Yellow
+        write-host
+
+        # Defining User Principal Name if not present
+        if($User -eq $null -or $User -eq ""){
+        $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
+        Write-Host
+        }
+        $global:authToken = Get-AuthToken -User $User
+    }
+} 
+# Authentication doesn't exist, calling Get-AuthToken function
+else { 
+    if($User -eq $null -or $User -eq ""){
+        $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
+        Write-Host
+    }
+    # Getting the authorization token
+    $global:authToken = Get-AuthToken -User $User
+}
+
+#endregion
+
+####################################################
+
+# For all valid Properties to Patch, go to https://developer.microsoft.com/en-us/graph/docs/api-reference/v1.0/api/alert_update 
+
+$JSON = @"    
+{
+  "status": "resolved"
+}
+"@
+
+
+Patch-Alert -JSON $JSON #-ID {Alert-id}
